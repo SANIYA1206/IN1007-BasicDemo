755160fa658aa3d447196f07a05a4732aec6a6cf
saniya sabesh <saniya.sabesh@city.ac.uk>
2026-02-19 19:44:27 +0000

    Fixed Ring Class Definition

--- src/game/Ring.java
+++ src/game/Ring.java
@@ -1,4 +1,12 @@
 package game;
 
-public class Ring {
+import city.cs.engine.*;
+
+public class Ring extends DynamicBody {
+
+    public Ring(World world) {
+        super(world, new CircleShape(0.6f));
+        addImage(new BodyImage("data/ring.png", 1.2f));
+        setGravityScale(0);
+    }
 }
