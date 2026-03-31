20b4fabd3b3d39875efc80dc419a6bf410e48795
saniya sabesh <saniya.sabesh@city.ac.uk>
2026-03-31 13:05:26 +0100

    Added Level3 with hardest obstacle layout

---
+++ src/game/Level3.java
@@ -0,0 +1,67 @@
+package game;
+
+import org.jbox2d.common.Vec2;
+
+public class Level3 extends GameLevel {
+
+    public Level3(Game game) {
+        super(game);
+        populate();
+    }
+
+    @Override
+    public void populate() {
+
+        Ring ring1 = new Ring(this);
+        ring1.setPosition(new Vec2(8f, 4f));
+
+        Ring ring2 = new Ring(this);
+        ring2.setPosition(new Vec2(15f, 2f));
+
+        Ring ring3 = new Ring(this);
+        ring3.setPosition(new Vec2(22f, 6f));
+
+        Bird bird1 = new Bird(this);
+        bird1.setPosition(new Vec2(10f, 6f));
+
+        Bird bird2 = new Bird(this);
+        bird2.setPosition(new Vec2(16f, 4f));
+
+        Bird bird3 = new Bird(this);
+        bird3.setPosition(new Vec2(22f, 5f));
+
+        Bird bird4 = new Bird(this);
+        bird4.setPosition(new Vec2(26f, 3f));
+
+        new RingOscillator(ring1);
+        new RingOscillator(ring2);
+
+        addStepListener(new Scroller(plane, ring1, true));
+        addStepListener(new Scroller(plane, ring2, true));
+        addStepListener(new Scroller(plane, ring3, true));
+        addStepListener(new Scroller(plane, bird1, false));
+        addStepListener(new Scroller(plane, bird2, false));
+        addStepListener(new Scroller(plane, bird3, false));
+        addStepListener(new Scroller(plane, bird4, false));
+    }
+
+    @Override
+    public boolean isComplete() {
+        return Game.score >= 20;
+    }
+
+    @Override
+    public GameLevel getNextLevel() {
+        return null;
+    }
+
+    @Override
+    public int getLevelNumber() {
+        return 3;
+    }
+
+    @Override
+    public String getBackgroundFile() {
+        return "data/sky3.png";
+    }
+}
