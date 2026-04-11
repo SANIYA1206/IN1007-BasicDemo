40d51aef8391496560794d19bebb9046317bf999
saniya sabesh <saniya.sabesh@city.ac.uk>
2026-04-11 15:08:51 +0100

    created a laser barrier for level 3 added the movements

---
+++ src/game/LaserBarrier.java
@@ -0,0 +1,28 @@
+package game;
+
+import city.cs.engine.BodyImage;
+import city.cs.engine.BoxShape;
+import city.cs.engine.Fixture;
+import city.cs.engine.StaticBody;
+import city.cs.engine.World;
+
+/**
+ * LaserBarrier represents an indestructible vertical laser
+ * that damages the player on contact.
+ */
+public class LaserBarrier extends StaticBody {
+
+    public LaserBarrier(World world) {
+        // Thin and tall shape for a vertical laser
+        super(world, new BoxShape(0.2f, 3f));
+
+        // Add the laser image (ensure this exists in the data folder)
+        addImage(new BodyImage("data/laser.png", 6f));
+
+        // Make the laser a sensor so the plane can pass through
+        // but still trigger collision events
+        for (Fixture fixture : getFixtures()) {
+            fixture.setSensor(true);
+        }
+    }
+}
--- src/game/Level2.java
+++ src/game/Level2.java
@@ -11,6 +11,8 @@ public class Level2 extends GameLevel {
 
     @Override
     public void populate() {
+
+        // Rings (collectibles)
         Ring ring1 = new Ring(this);
         ring1.setPosition(new Vec2(8f, 4f));
 
@@ -20,21 +22,25 @@ public class Level2 extends GameLevel {
         Ring ring3 = new Ring(this);
         ring3.setPosition(new Vec2(20f, 5f));
 
+        // Birds (enemies)
         Bird bird1 = new Bird(this);
         bird1.setPosition(new Vec2(10f, 6f));
 
         Bird bird2 = new Bird(this);
         bird2.setPosition(new Vec2(16f, 3f));
 
+        // Spikes (new hazard introduced in Level 2)
         Spike spike1 = new Spike(this);
         spike1.setPosition(new Vec2(6f, 0f));
 
         Spike spike2 = new Spike(this);
         spike2.setPosition(new Vec2(12f, 1f));
 
+        // Oscillating rings for added difficulty
         new RingOscillator(ring1);
         new RingOscillator(ring2);
 
+        // Scrolling behaviour
         addStepListener(new Scroller(plane, ring1, true));
         addStepListener(new Scroller(plane, ring2, true));
         addStepListener(new Scroller(plane, ring3, true));
@@ -44,11 +50,13 @@ public class Level2 extends GameLevel {
         addStepListener(new Scroller(plane, spike2, false));
     }
 
+    // Level completion condition
     @Override
     public boolean isComplete() {
         return Game.score >= 10;
     }
 
+    // Progress to Level 3
     @Override
     public GameLevel getNextLevel() {
         return new Level3(game);
@@ -59,8 +67,9 @@ public class Level2 extends GameLevel {
         return 2;
     }
 
+    // Sunset background
     @Override
     public String getBackgroundFile() {
-        return "data/sky.png";
+        return "data/sky2.png";
     }
 }
